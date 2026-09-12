b117c53f54ac64340994f17031ec8b1b80e7be8e
Mukunda <mukundaramachary.123@gmail.com>
2026-09-12 19:25:22 +0530

    fix(build): add baseUrl to tsconfig and resolve glass components

---
+++ components/glass/GlassProgress.tsx
@@ -0,0 +1,3 @@
+"use client";
+
+export { GlassProgress, type GlassProgressProps } from "./GlassBadge";
---
+++ components/glass/index.ts
@@ -0,0 +1,16 @@
+"use client";
+
+export * from "./GlassCard";
+export * from "./GlassButton";
+export * from "./GlassBadge";
+export * from "./GlassProgress";
+export * from "./GlassInput";
+export * from "./GlassSelect";
+export * from "./GlassModal";
+export * from "./GlassNavbar";
+export * from "./GlassSidebar";
+export * from "./GlassBottomNavigation";
+export * from "./GlassCommandMenu";
+export * from "./GlassAlert";
+export * from "./GlassUpload";
+export * from "./GlassChartContainer";
